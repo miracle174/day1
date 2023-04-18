9ed41e4c3dd640eb3559488b450eb16f9b94dfe9
miracle174 <miracleog37@gmail.com>
2023-04-18 08:36:04 +0800

    #uurchilult oruulav

--- day.js
+++ day.js
@@ -1,5 +1,8 @@
-let a = 1;
-let b = 1;
-let c = a + b;
+function sum(a, b) {
+    console.log(a+b);
+    return a + b;
+}
 
-console.log(c)
+let result  = sum(1, 2)
+result = sum (result, 3)
+console.log('Final result: ', result)
